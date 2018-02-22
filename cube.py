32e3f10f598cea039217418acf1cf48f90c5f6ff
Anthony Bryant <anthony@bryants.eu>
2018-02-22 00:41:38 +0000

    Move Pos and Direction to position.py, add a Grid class

--- cube.py
+++ cube.py
@@ -8,52 +8,7 @@
 #
 import generators
 from display import *
-from enum import Enum
-
-SIZE = 4
-
-class Pos:
-  def __init__(self, x, y, z):
-    self.x = x
-    self.y = y
-    self.z = z
-
-  def is_in_bounds(self, size = SIZE):
-    return self.x >= 0 and self.x < size and self.y >= 0 and self.y < size and self.z >= 0 and self.z < size
-
-  def __add__(self, other):
-    return Pos(self.x + other.x, self.y + other.y, self.z + other.z)
-
-  def __sub__(self, other):
-    return Pos(self.x - other.x, self.y - other.y, self.z - other.z)
-
-  def __mul__(self, other):
-    if type(other) is int:
-      return Pos(self.x * other, self.y * other, self.z * other)
-    return Pos(self.x * other.x, self.y * other.y, self.z * other.z)
-
-  def __neg__(self):
-    return Pos(-self.x, -self.y, -self.z)
-
-  def __eq__(self, other):
-    return self.x == other.x and self.y == other.y and self.z == other.z
-
-  def __ne__(self, other):
-    return not self.__eq__(other)
-
-  def __str__(self):
-    return "[" + str(self.x) + "," + str(self.y) + "," + str(self.z) + "]"
-
-  def __repr__(self):
-    return "[" + str(self.x) + "," + str(self.y) + "," + str(self.z) + "]"
-
-class Direction(Enum):
-  UP = Pos(0, -1, 0)
-  DOWN = Pos(0, 1, 0)
-  LEFT = Pos(-1, 0, 0)
-  RIGHT = Pos(1, 0, 0)
-  FRONT = Pos(0, 0, 1)
-  BACK = Pos(0, 0, -1)
+from position import *
 
 def get_perpendicular_direction(dir1, dir2):
   if (dir1 == Direction.UP or dir1 == Direction.DOWN) and (dir2 == Direction.LEFT or dir2 == Direction.RIGHT):
