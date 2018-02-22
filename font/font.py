fcef12c13c3cc1537b03b4919547ab7ef9fd242e
Anthony Bryant <anthony@bryants.eu>
2018-02-22 00:40:16 +0000

    Add a font module

---
+++ font/font.py
@@ -0,0 +1,14 @@
+from .font_arrays import font_arrays
+
+def print_character(c):
+  arr = font_arrays[c]
+  for y in range(8):
+    for x in range(8):
+      print('X' if (arr[y] >> x) & 1 else ' ', end='')
+    print('')
+
+def draw_character(c, on, off):
+  """Draws the character c into an 8x8 array[y][x], with pixels set to on and off."""
+  arr = font_arrays[c]
+  return [[(on if (arr[y] >> x) & 1 else off) for x in range(8)] for y in range(8)]
+
