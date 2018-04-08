a83d89c3e1c691ea3a2fb7f4d63a6c0a56ef44bd
Anthony Bryant <anthony@bryants.eu>
2018-04-08 22:32:09 +0000

    Allow newline bytes in client.py

--- client/client.py
+++ client/client.py
@@ -2,11 +2,22 @@ import requests
 import sys
 from display import *
 
-def process_line(line, display):
-  print("Received: " + repr(line))
-  if len(line) > 7 and line.startswith(b'CUBE:'):
-    length = ((line[5] << 8) | line[6])
-    data = line[7:7+(3*length)]
+def check_prefix(infile):
+  first = infile.read(1)
+  if first == b'':
+    raise EOFError()
+  if first == b'C':
+    if infile.read(1) == b'U':
+      if infile.read(1) == b'B':
+        if infile.read(1) == b'E':
+          if infile.read(1) == b':':
+            return True
+  return False
+
+def process_line(infile, display):
+  if check_prefix(infile):
+    length = ((infile.read(1)[0] << 8) | infile.read(1)[0])
+    data = infile.read(length * 3)
     colours = []
     for i in range(length):
       r = data[3*i]
@@ -14,12 +25,18 @@ def process_line(line, display):
       b = data[3*i + 2]
       colours.append(Colour((r, g, b)))
     display.display(colours)
+  while infile.read(1) != b'\n':
+    pass
 
 def main(url):
   with Display() as d:
-    r = requests.get(url, stream=True)
-    for line in r.iter_lines():
-      process_line(line, d)
+    try:
+      r = requests.get(url, stream=True)
+      while True:
+        process_line(r.raw, d)
+    except EOFError:
+      print("End of stream - stopping.")
+      pass
 
 if __name__ == "__main__":
   main(sys.argv[1])
