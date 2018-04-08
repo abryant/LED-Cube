98825b96baa48f99a94dca0f63e4103b58ededd5
Anthony Bryant <anthony@bryants.eu>
2018-04-08 22:35:55 +0000

    Make the server send cube visuals

--- server/controller.py
+++ server/controller.py
@@ -1,18 +1,30 @@
 from queue import Queue, Empty
 from time import sleep
+import generators
+from visuals import corners, cube_visuals, extend, flash, flatten, matrix, pulse, rainbow, rotate, shuffle, sides, snakes, tetris
 
-def asdf():
-  while True:
-    yield 'a'
-    yield 's'
-    yield 'd'
-    yield 'f'
+GENERATORS = {
+  'corners': corners.corners,
+  'cube_visuals': cube_visuals.cube_visuals,
+  'extend': extend.extend,
+  'flash': flash.flash,
+  'flatten': flatten.flatten,
+  'matrix': matrix.matrix,
+  'pulse': pulse.pulse,
+  'rainbow': rainbow.scroll_diagonal_rainbow,
+  'rotate': rotate.rotate,
+  'shuffle': shuffle.shuffle,
+  'sides': sides.sides,
+  'snakes': snakes.snakes,
+  'tetris': tetris.tetris,
+}
 
 class Controller:
   def __init__(self, queue, file):
     self.queue = queue
     self.file = file
-    self.delay = 1
+    self.delay = 0.05
+    self.brightness = 50
     self.current_generator = None
     self.stopped = False
 
@@ -37,10 +49,22 @@ class Controller:
       if self.stopped:
         return
       if self.current_generator is not None:
-        self.send(next(self.current_generator) + '\n')
+        self.send_frame()
         sleep(self.delay)
       else:
         self.send('.\n')
+
+  def send_frame(self):
+    try:
+      while True:
+        frame = next(self.current_generator)
+        frame = generators.get_colours(frame)
+        if type(frame) is list:
+          frame_bytes = bytes([b for c in frame for b in [c.r, c.g, c.b]])
+          self.send(b'CUBE:' + bytes([len(frame) >> 8, len(frame) & 0xff]) + frame_bytes + b'\n')
+          return
+    except StopIteration:
+      self.current_generator = None
 
   def process_command(self, command):
     if command == 'quit':
@@ -50,13 +74,23 @@ class Controller:
     if command.startswith('delay=') and len(command) > len('delay='):
       try:
         d = float(command[len('delay='):])
-        if d > 0:
+        if d >= 0.015:
           self.delay = d
       except ValueError:
         pass
       return
-    if command == 'asdf':
-      self.current_generator = asdf()
+    if command.startswith('brightness=') and len(command) > len('brightness='):
+      try:
+        b = int(command[len('brightness='):])
+        if b >= 0 and b < 256:
+          self.brightness = b
+      except ValueError:
+        pass
+      return
+    if command.startswith('start:'):
+      name = command[len('start:'):]
+      if name in GENERATORS:
+        self.current_generator = GENERATORS[name]()
       return
     if command == 'stop':
       self.current_generator = None
