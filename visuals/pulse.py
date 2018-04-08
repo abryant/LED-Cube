05eec491ee4ce9afbcf86b591b4fdc3c3181c493
Anthony Bryant <anthony@bryants.eu>
2018-04-08 22:29:05 +0000

    Make pulse.pulse() generate a sequence of colours

--- visuals/pulse.py
+++ visuals/pulse.py
@@ -5,7 +5,7 @@ import functools
 def fade(brightness, colour):
   return Colour((colour.r * brightness, colour.g * brightness, colour.b * brightness))
 
-def pulse(cube):
+def pulse_cube(cube):
   while True:
     for i in range(21):
       c = cube.copy()
@@ -17,12 +17,15 @@ def pulse(cube):
       yield c
     yield True
 
+def pulse():
+  return generators.sequence([
+      pulse_cube(Cube(colour = Colour((255, 0, 0)))),
+      pulse_cube(Cube(colour = Colour((0, 255, 0)))),
+      pulse_cube(Cube(colour = Colour((0, 0, 255)))),
+      pulse_cube(Cube(colour = Colour((255, 255, 255)))),
+    ])
+
 if __name__ == "__main__":
   with Display() as d:
-    generators.generate(d, generators.sequence([
-      pulse(Cube(colour = Colour((255, 0, 0)))),
-      pulse(Cube(colour = Colour((0, 255, 0)))),
-      pulse(Cube(colour = Colour((0, 0, 255)))),
-      pulse(Cube(colour = Colour((255, 255, 255)))),
-    ]))
+    generators.generate(d, pulse())
 
