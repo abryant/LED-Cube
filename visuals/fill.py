5a38032f6463f316d198ee5afa5bd074bd2b4a74
daliasen <18193042+daliasen@users.noreply.github.com>
2020-01-26 21:43:26 +0000

    make the 'fill' visual faster and colorful

--- visuals/fill.py
+++ visuals/fill.py
@@ -3,16 +3,17 @@ import random
 
 def fill():
   c = Cube()
-  colour = Colour.cyan()
   on = True
   while True:
+    colour = hue_to_colour(random.randint(0, 359))
     all_coords = [(x, y, z) for x in range(c.size) for y in range(c.size) for z in range(c.size)]
     random.shuffle(all_coords)
-    for coord in all_coords:
+    for counter, coord in enumerate(all_coords):
         if on:
           c.set(Pos(*coord), colour)
         else:
           c.set(Pos(*coord), Colour.BLACK)
-        yield c.copy()
+        if counter % c.size == 7:
+          yield c.copy()
     on = not on
     yield True
